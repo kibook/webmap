0a9d903b509f721b955ae351deca4e632ae62491
kibook <kibuzhj@gmail.com>
2021-08-12 09:48:53 -0400

    Add weathersync dependency

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,8 @@ fx_version "cerulean"
 game "rdr3"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
+dependency "weathersync"
+
 server_scripts {
 	"config.lua",
 	"server.lua"
